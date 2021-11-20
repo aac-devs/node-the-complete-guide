977fbdb2025542ec3174e518d13dc15cfba0d94d
aac-devs <aac.devs@gmail.com>
2021-11-20 13:10:07 -0300

    section 9 started

---
+++ section9/controllers/admin.js
@@ -0,0 +1,60 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  console.log(req.body);
+  const { title, imageUrl, description, price } = req.body;
+  const product = new Product(null, title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product,
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  console.log(req.body);
+  const { productId, title, imageUrl, price, description } = req.body;
+  const updatedProduct = new Product(
+    productId,
+    title,
+    imageUrl,
+    description,
+    price
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
+    });
+  });
+};
